df98455d08210520b3715d420d4cb0d4e65b172d
Vitaliy Timchenko <vitaliy.timchenko@gmail.com>
2016-04-24 19:19:42 +0300

    Clean up code. Leave basic auth only

---
+++ src/main/java/com/we2seek/client/Index.java
@@ -0,0 +1,13 @@
+package com.we2seek.client;
+
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+
+public class Index implements EntryPoint {
+
+    public void onModuleLoad() {
+        RootPanel.get().add(new Label("GWT: It works..."));
+    }
+}
